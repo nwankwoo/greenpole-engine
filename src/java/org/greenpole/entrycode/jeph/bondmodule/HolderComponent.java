6fb49c32c37e9ddc85eac1d94fb28378c0a327a5
Jephthah Sadare <jephsadare@gmail.com>
2015-04-21 08:35:06 +0100

    Implemented Holder component

--- src/java/org/greenpole/entrycode/jeph/bondmodule/HolderComponent.java
+++ src/java/org/greenpole/entrycode/jeph/bondmodule/HolderComponent.java
@@ -8,18 +8,18 @@ package org.greenpole.entrycode.jeph.bondmodule;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.JAXBException;
+import org.greenpole.entity.model.jeph.models.holder.Holder;
 import org.greenpole.entity.notification.NotificationMessageTag;
 import org.greenpole.entity.notification.NotificationWrapper;
 import org.greenpole.entity.response.Response;
 import org.greenpole.entity.security.Login;
-import org.greenpole.entrycode.jeph.models.Holder;
-import org.greenpole.entrycode.jeph.models.PrivatePlacement;
+import org.greenpole.hibernate.entity.*;
+import org.greenpole.entity.model.jeph.models.holder.*;
 import org.greenpole.hibernate.entity.HolderCompanyAccount;
 import org.greenpole.hibernate.entity.HolderPhoneNumber;
 import org.greenpole.hibernate.entity.HolderPostalAddress;
 import org.greenpole.hibernate.entity.HolderResidentialAddress;
 import org.greenpole.hibernate.query.HolderComponentQuery;
-import org.greenpole.hibernate.query.factory.ComponentQueryFactory;
 import org.greenpole.hibernate.query.impl.HolderComponentQueryImpl;
 import org.greenpole.notifier.sender.QueueSender;
 import org.greenpole.util.Notification;
@@ -80,9 +80,9 @@ public class HolderComponent {
             return res;
         }
 
-        res.setRetn(200);
-        res.setDesc("holder account already exists and so cannot be created");
-        logger.info("holder account already exists and so cannot be created - [{}] [{}]", hold.getFirstName(), hold.getLastName());
+        // res.setRetn(200);
+        // res.setDesc("holder account already exists and so cannot be created");
+        // logger.info("holder account already exists and so cannot be created - [{}] [{}]", hold.getFirstName(), hold.getLastName());
         return res;
     }
 
@@ -94,7 +94,7 @@ public class HolderComponent {
             List<Holder> holdList = (List<Holder>) wrapper.getModel();
             Holder holdModel = holdList.get(0);
             org.greenpole.hibernate.entity.Holder holdEntity = new org.greenpole.hibernate.entity.Holder();
-            holdEntity.setId(holdModel.getId());
+            // holdEntity.setId(holdModel.getId());
             // holdEntity.setHolder(holdModel.getHolder());
             holdEntity.setFirstName(holdModel.getFirstName());
             holdEntity.setLastName(holdModel.getLastName());
@@ -102,15 +102,24 @@ public class HolderComponent {
             holdEntity.setType(holdModel.getType());
             holdEntity.setGender(holdModel.getGender());
             holdEntity.setDob(holdModel.getDob());
+            holdEntity.setChn(holdModel.getChn());
             
-            boolean created = ch.createHolderAccount(holdEntity, createHolderCompanyAccount(holdModel), createHolderPostalAddress(holdModel), createHolderPhoneNumber(holdModel));
+            boolean created;
+            // determine if residential address was set or postal address
+            if (holdModel.getHolderResidentialAddresses().isEmpty()) {
+                created = ch.createHolderAccount(holdEntity, retrieveHolderCompanyAccount(holdModel), retrieveHolderPostalAddress(holdModel), retrieveHolderPhoneNumber(holdModel));
+            } else {
+                // boolean created = ch.createHolderAccount(holdEntity, retrieveHolderCompanyAccount(holdModel), retrieveHolderPostalAddress(holdModel), retrieveHolderPhoneNumber(holdModel));
+                created = ch.createHolderAccount(holdEntity, retrieveHolderCompanyAccount(holdModel), retrieveHolderResidentialAddress(holdModel), retrieveHolderPhoneNumber(holdModel));
+            }
+            
             if (created) {
                 res.setRetn(0);
                 res.setDesc("Successful Persistence");
                 return res;
             } else {
                 res.setRetn(200);
-                res.setDesc("An error occured persisting the data");
+                res.setDesc("An error occured persisting the data residential and postal addresses are empty");
                 return res;
             }
 
@@ -120,43 +129,89 @@ public class HolderComponent {
             res.setDesc("error loading notification xml file. See error log");
             logger.info("error loading notification xml file. See error log");
             logger.error("error loading notification xml file to object - ", ex);
+        } catch (Exception ex) {
+            res.setRetn(100);
+            res.setDesc("error. See error log");
+            logger.info("error. See error log");
+            logger.error("error - ", ex);
         }
         return res;
 
     }
 
-    private Holder retrieveHolderModel(Holder holdModel) {
-
-        return holdModel;
-    }
-
-    private HolderCompanyAccount createHolderAccount(Holder holdModel) {
-        org.greenpole.hibernate.entity.HolderCompanyAccount companyAccountEntity = new org.greenpole.hibernate.entity.HolderCompanyAccount();
-        
-        return companyAccountEntity;
-    }
-
-    private HolderPostalAddress createHolderPostalAddress(Holder holdModel) {
+    private HolderPostalAddress retrieveHolderPostalAddress(Holder holdModel) {
         org.greenpole.hibernate.entity.HolderPostalAddress postalAddressEntity = new org.greenpole.hibernate.entity.HolderPostalAddress();
+        List<org.greenpole.entity.model.jeph.models.holder.HolderPostalAddress> hpaddyList = holdModel.getHolderPostalAddresses();
+        List<org.greenpole.hibernate.entity.HolderPostalAddress> returnHolderPostalAddress = new ArrayList<>();
         
-        return postalAddressEntity;
+        for (org.greenpole.entity.model.jeph.models.holder.HolderPostalAddress hpa : hpaddyList) {
+            HolderPostalAddressId postalAddyId = new HolderPostalAddressId();
+            postalAddyId.setHolderId(hpa.getHolderId());
+            postalAddressEntity.setId(postalAddyId);
+            postalAddressEntity.setIsPrimary(hpa.isIsPrimary());
+            returnHolderPostalAddress.add(postalAddressEntity);
+        }
+        return (HolderPostalAddress) returnHolderPostalAddress;
     }
 
-    private HolderPhoneNumber createHolderPhoneNumber(Holder holdModel) {
+    private HolderPhoneNumber retrieveHolderPhoneNumber(Holder holdModel) {
+        
         org.greenpole.hibernate.entity.HolderPhoneNumber phoneNumberEntity = new org.greenpole.hibernate.entity.HolderPhoneNumber();
+        List<org.greenpole.entity.model.jeph.models.holder.HolderPhoneNumber> phoneNumberList = holdModel.getHolderPhoneNumbers();
+        List<org.greenpole.hibernate.entity.HolderPhoneNumber> returnPhoneNumber = new ArrayList<>();
         
-        return phoneNumberEntity;
+        for (org.greenpole.entity.model.jeph.models.holder.HolderPhoneNumber pnList : phoneNumberList) {
+            HolderPhoneNumberId phoneNoId = new HolderPhoneNumberId();
+            phoneNoId.setHolderId(pnList.getHolderId());
+            
+            phoneNumberEntity.setId(phoneNoId);
+            // phoneNumberEntity.setIsPrimary(phoneNoId);
+        }
+        return (HolderPhoneNumber) returnPhoneNumber;
     }
 
-    private HolderResidentialAddress createHolderResidentialAddress(Holder holdModel) {
+    private HolderResidentialAddress retrieveHolderResidentialAddress(Holder holdModel) {
         org.greenpole.hibernate.entity.HolderResidentialAddress residentialAddressEntity = new org.greenpole.hibernate.entity.HolderResidentialAddress();
+        List<org.greenpole.entity.model.jeph.models.holder.HolderResidentialAddress> residentialAddressList = holdModel.getHolderResidentialAddresses();
+        List<org.greenpole.hibernate.entity.HolderResidentialAddress> returnResidentialAddress = new ArrayList();
         
-        return residentialAddressEntity;
+        for (org.greenpole.entity.model.jeph.models.holder.HolderResidentialAddress rAddy : residentialAddressList) {
+            HolderResidentialAddressId rAddyId = new HolderResidentialAddressId();
+            rAddyId.setHolderId(rAddy.getHolderId());
+            rAddyId.setAddressLine1(rAddy.getAddressLine1());
+            rAddyId.setState(rAddy.getState());
+            rAddyId.setCountry(rAddy.getCountry());
+            
+            residentialAddressEntity.setId(rAddyId);
+            residentialAddressEntity.setAddressLine2(rAddy.getAddressLine2());
+            residentialAddressEntity.setAddressLine3(rAddy.getAddressLine3());
+            residentialAddressEntity.setAddressLine4(rAddy.getAddressLine4());
+            residentialAddressEntity.setCity(rAddy.getCity());
+            residentialAddressEntity.setPostCode(rAddy.getPostCode());
+            
+            returnResidentialAddress.add(residentialAddressEntity);
+        }
+        
+        return (HolderResidentialAddress)returnResidentialAddress;
+        
     }
 
-    private HolderCompanyAccount createHolderCompanyAccount(Holder holdModel) {
+    private HolderCompanyAccount retrieveHolderCompanyAccount(Holder holdModel) {
         org.greenpole.hibernate.entity.HolderCompanyAccount companyAccountEntity = new org.greenpole.hibernate.entity.HolderCompanyAccount();
+        List<org.greenpole.entity.model.jeph.models.holder.HolderCompanyAccount> companyAccountList = holdModel.getHolderCompanyAccounts();
+        List<org.greenpole.hibernate.entity.HolderCompanyAccount> returnCompanyAccountList = new ArrayList();
         
-        return companyAccountEntity;
+        for (org.greenpole.entity.model.jeph.models.holder.HolderCompanyAccount compAcct : companyAccountList) {
+            HolderCompanyAccountId hCompAcctId = new HolderCompanyAccountId();
+            hCompAcctId.setHolderId(compAcct.getHolderId());
+            hCompAcctId.setClientCompanyId(compAcct.getClientCompanyId());
+            
+            // companyAccountEntity.setBank(compAcct.getBankId());
+            companyAccountEntity.setChn(compAcct.getChn());
+            companyAccountEntity.setId(hCompAcctId);
+            returnCompanyAccountList.add(companyAccountEntity);
+        }
+        
+        return (HolderCompanyAccount) returnCompanyAccountList;
     }
 }
