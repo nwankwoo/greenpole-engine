eeb7f00f95577b3280f38c5abbdc09076bba1687
Jephthah Sadare <jephsadare@gmail.com>
2015-05-06 16:20:00 +0100

    Correction on BondComponent, HolderComponent and PrivatePlacementComponent

--- src/java/org/greenpole/entrycode/jeph/bondmodule/BondComponent.java
+++ src/java/org/greenpole/entrycode/jeph/bondmodule/BondComponent.java
@@ -5,6 +5,8 @@
  */
 package org.greenpole.entrycode.jeph.bondmodule;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import javax.xml.bind.JAXBException;
 import org.greenpole.entity.model.jeph.models.payment.Bond;
@@ -32,6 +34,7 @@ public class BondComponent {
 
     private final ClientCompanyComponentQuery cq = ComponentQueryFactory.getClientCompanyQuery();
     private static final Logger logger = LoggerFactory.getLogger(ClientCompanyComponentLogic.class);
+    SimpleDateFormat formatter = new SimpleDateFormat();
 
     /**
      * process request to create a type of bond as a bond offer
@@ -49,48 +52,52 @@ public class BondComponent {
         NotificationWrapper wrapper;
         QueueSender queue;
         NotifierProperties prop;
-        
+        String resDesc = null;
+        boolean flag = false;
+
         // ClientCompany cc = cq.getClientCompany(bond.getClientCompanyId());
         // checks if the bond was entered correctly
         try {
             // Check if bond title has a value
             if (bond.getTitle().isEmpty() || bond.getTitle() == null) {
-                res.setRetn(201);
-                res.setDesc("Bond title is missing");
-                logger.error("Bond title is missing");
-                
-                return res;
+                resDesc += "\nBond title should not be empty";
+            } else if (bond.getBondType() == null || "".equals(bond.getBondType())) {
+                resDesc += "\nBond type should not be empty";
+            } else if (bond.getBondUnitPrice() <= 0) {
+                resDesc += "\nBond unit price should be greater than zero";
+            } else if (bond.getInterestRate() <= 0) {
+                resDesc += "\nBond interest rate should be greater than zero";
+            } else if (bond.getPaymentPlan() == null || "".equals(bond.getPaymentPlan())) {
+                resDesc += "\nBond payment plan should be specified";
+            } else {
+                flag = true;
             }
-            if (null == bond) {
-                res.setRetn(202);
-                res.setDesc("No bond object available");
-                logger.info("No bond object available");
-                
-                return res;
+
+            if (flag) {
+                wrapper = new NotificationWrapper();
+                prop = new NotifierProperties(BondComponent.class);
+                queue = new QueueSender(prop.getAuthoriserNotifierQueueFactory(), prop.getAuthoriserNotifierQueueName());
+                List<Bond> bc = new ArrayList<>();
+                wrapper.setCode(Notification.createCode(login));
+                wrapper.setDescription("Creates Bond " + bond.getTitle());
+                wrapper.setMessageTag(NotificationMessageTag.Authorisation_request.toString());
+                wrapper.setFrom(login.getUserId());
+                wrapper.setTo(authenticator);
+                wrapper.setModel(bc);
+                res = queue.sendAuthorisationRequest(wrapper);
+                logger.info("notification fowarded to queue - notification code: [{}]", wrapper.getCode());
+            } else {
+                res.setRetn(200);
+                res.setDesc(resDesc);
+                logger.info(resDesc);
             }
-            wrapper = new NotificationWrapper();
-            prop = new NotifierProperties(BondComponent.class);
-            queue = new QueueSender(prop.getAuthoriserNotifierQueueFactory(), prop.getAuthoriserNotifierQueueName());
-            
-            List<BondComponent> bc = new ArrayList<>();
-
-            wrapper.setCode(Notification.createCode(login));
-            wrapper.setDescription("Creates Bond " + bond.getTitle());
-            wrapper.setMessageTag(NotificationMessageTag.Authorisation_request.toString());
-            wrapper.setFrom(login.getUserId());
-            wrapper.setTo(authenticator);
-            wrapper.setModel(bc);
-
-            res = queue.sendAuthorisationRequest(wrapper);
-            logger.info("notification fowarded to queue - notification code: [{}]", wrapper.getCode());
             return res;
-
         } catch (Exception npx) {
             // TODO: change from Exception class to specific user-defined exceptions later
             // TODO: catch other types of exception
             res.setRetn(200);
-            res.setDesc("Bond offer already exists or has empty parameters and so cannot be created.");
-            logger.info("Bond offer exists or has empty parameters and so cannot be created - [{}]: [{}]", bond.getTitle(), res.getRetn());
+            res.setDesc("Error creating bond offer");
+            logger.info("Error creating bond offer");
         }
         return res;
     }
@@ -103,7 +110,7 @@ public class BondComponent {
      * @return response object back to sender indicating authorization request
      * status
      */
-    public Response setupBondOffer_Authorise(String notificationCode) {
+    public Response setupBondOffer_Authorise(String notificationCode) throws ParseException {
         Response res = new Response();
         logger.info("bond setup creation authorised - [{}]", notificationCode);
         try {
@@ -118,15 +125,19 @@ public class BondComponent {
             res.setDesc("Successful persistence");
 
             return res;
+        } catch (ParseException pex) {
+            res.setRetn(100);
+            res.setDesc("error converting string to date type. See error log");
+            logger.info("error converting string to date type. See error log");
+            logger.error("error converting string to date type - ", pex);
         } catch (JAXBException ex) {
             // TODO: catch other types of exception
             res.setRetn(100);
             res.setDesc("error loading notification xml file. See error log");
             logger.info("error loading notification xml file. See error log");
             logger.error("error loading notification xml file to object - ", ex);
-
-            return res;
         }
+        return res;
     }
 
     /**
@@ -137,12 +148,12 @@ public class BondComponent {
      * @param bondModel representing a bond model object
      * @return BondOffer object to setupBondOfferAuthorise method
      */
-    private BondOffer bondCreationMain(Bond bondModel) {
+    private BondOffer bondCreationMain(Bond bondModel) throws ParseException {
         // instantiate required hibernate entities
         org.greenpole.hibernate.entity.BondOffer bond_main = new org.greenpole.hibernate.entity.BondOffer();
         // TODO incorporate client company info into the database
         bond_main.setTitle(bondModel.getTitle());
-        bond_main.setBondMaturity((Date) bondModel.getBondMaturity());
+        bond_main.setBondMaturity(formatter.parse(bondModel.getBondMaturity()));
         bond_main.setBondUnitPrice(bondModel.getBondUnitPrice());
         bond_main.setBondType(bondModel.getBondType());
         bond_main.setInterestRate(bondModel.getInterestRate());
